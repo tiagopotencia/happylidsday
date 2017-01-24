437a933ab61b36a109f6e65ba72ba0204a328310
Tiago Moura Brandão <Tiago Moura Brando>
2017-01-24 04:25:03 -0200

    Arranging project. All files to their own place.

---
+++ js/lids.js
@@ -0,0 +1,25 @@
+// init bunch of sounds
+ion.sound({
+    sounds: [
+        {
+            name: "lids",
+            preload: true
+        },
+    ],
+    // main config
+    path: "sounds/",
+    preload: true,
+    multiplay: false,
+    volume: 0.9,
+    
+    ready_callback: hey
+});
+
+function hey(){
+     $('#btnloading').hide();
+     $('#btnLids').show();
+}
+// play sound
+function play(){
+    ion.sound.play("lids");
+}
